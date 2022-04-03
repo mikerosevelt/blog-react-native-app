987093a3bb1912dc7de0a42387a8ce23a023c7bf
Meidhy <meidhy@klinikpintar.id>
2022-04-03 21:39:29 +0700

    initial commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,65 @@
+import React, { useContext } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity,
+} from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+const IndexScreen = ({ navigation }) => {
+	const { state, deleteBlogPost } = useContext(Context);
+	return (
+		<View>
+			<FlatList
+				data={state}
+				keyExtractor={(blogPost) => blogPost.title}
+				renderItem={({ item }) => {
+					return (
+						<TouchableOpacity
+							onPress={() => navigation.navigate('Show', { id: item.id })}
+						>
+							<View style={styles.row}>
+								<Text style={styles.title}>{item.title}</Text>
+								<TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+									<Feather style={styles.icon} name="trash-2" />
+								</TouchableOpacity>
+							</View>
+						</TouchableOpacity>
+					);
+				}}
+			/>
+		</View>
+	);
+};
+
+IndexScreen.navigationOptions = () => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity onPress={() => navigation.navigate('Create')}>
+				<Feather name="plus" size={30} />
+			</TouchableOpacity>
+		),
+	};
+};
+
+const styles = StyleSheet.create({
+	row: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingVertical: 20,
+		paddingHorizontal: 10,
+		borderTopWidth: 1,
+		borderColor: 'gray',
+	},
+	title: {
+		fontSize: 18,
+	},
+	icon: {
+		fontSize: 24,
+	},
+});
+
+export default IndexScreen;
